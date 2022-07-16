ed126187b4df4a08e45476b6e88c691db1f91049
PD <cosmos.lab.contact@gmail.com>
2022-07-17 00:45:32 +0530

    Refactor

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./views/App";
 
 const test = 2;
+const test2 = 2;
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
